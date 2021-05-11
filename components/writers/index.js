c2554267750f3b148971c361789a54eba36079e4
Cameron Leverett <camrbo@gmail.com>
2021-05-11 12:29:46 -0700

    added export for writer component

--- components/writers/index.js
+++ components/writers/index.js
@@ -1,3 +1,4 @@
 export { default as WriterBio } from './writer-bio'
 export { default as WriterHeadline } from './writer-headline'
 export { default as WriterImage } from './writer-image'
+export { default as Writer } from './writer'
