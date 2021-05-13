6040a3794a1dca3a1b8233e35c8fe7ce60d18365
Cameron Leverett <camrbo@gmail.com>
2021-05-12 18:07:43 -0700

    added the import for layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,14 +1,17 @@
-import { Fragment } from 'react'
-import { Navigation } from '@/navigation/*'
+import { Fragment } from 'react';
+import { Navigation } from '@/navigation/*';
+import Footer from './footer';
+import styles from '@/styles/main.module.css'
 
 const Layout = (props) => {
-  const { children } = props
+  const { children } = props;
   return (
     <Fragment>
       <Navigation />
-      {children}
+      <div className={styles.main}>{children}</div>
+      <Footer />
     </Fragment>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
