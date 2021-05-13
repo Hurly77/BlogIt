59d54540296e5d6224cd329ad17ba29d225eed63
Cameron Leverett <camrbo@gmail.com>
2021-05-12 18:16:29 -0700

    added Featured component/ added get static props

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import { Writer } from '@/writer/*'
+import { Writer, Featured } from '@/writer/*'
 
-const HomePage = () => {
+const HomePage = ({ stories }) => {
   return (
     <div>
       <Writer />
+      <Featured stories={stories} />
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://blogit-cf715-default-rtdb.firebaseio.com/stories.json')
+  const json = await res.json()
+  const data = await json.map(story => story)
+
+  return {
+    props: { stories: data }
+  }
 }
 
 export default HomePage
