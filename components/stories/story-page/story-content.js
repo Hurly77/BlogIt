b032d7f45681da1c0bdd3fe0ed76330cc3d4ad77
Cameron Leverett <camrbo@gmail.com>
2021-05-13 14:33:07 -0700

    added  comonent for jounal/id, for head of page

---
+++ components/stories/story-page/story-content.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+const StoryConent = () => {
+  return (
+    <article>
+      <StoryHead />
+      <p>{content}</p>
+    </article>
+  )
+}
+
+export default StoryConent
