af8d49b7b3e393fca333cab4aaa7626751ca9ee8
Cameron Leverett <camrbo@gmail.com>
2021-05-12 18:13:18 -0700

    added full story component for story cards

--- components/stories/story.js
+++ components/stories/story.js
@@ -1,10 +1,32 @@
+import Link from 'next/link';
+import styles from '@/styles/story-item.module.css';
+import Image from 'next/image';
 
-const Story = () => {
+const Story = (props) => {
+  const { title, date, content, image, slug } = props;
+
+  const formatedDate = new Date(date).toLocaleString('en-Us', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <li className={styles.story}>
+      <Link href={slug}>
+        <a>
+          <div className={styles.image}>
+            <Image src={`/images/${image}`} width={300} height={200} layout="responsive" />
+          </div>
+          <div className={styles.content}>
+            <h3>{title}</h3>
+            <time>{formatedDate}</time>
+            <p>{content}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
+};
 
-export default Story
+export default Story;
