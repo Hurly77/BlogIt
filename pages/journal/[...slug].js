a1afc515e74c06bb599d8990deaba0cbae1514d8
Cameron Leverett <camrbo@gmail.com>
2021-05-17 23:37:39 -0700

    added nested route for sinlge jounal entry

--- pages/journal/[...slug].js
+++ pages/journal/[...slug].js
@@ -1,11 +1,35 @@
-import React from 'react'
+import {
+  StoryHead,
+  StoryContent,
+  Story,
+} from '@/stories/*'
 
-const StoryPage  = () => {
+const StoryPage = (props) => {
+  console.log(props)
   return (
     <div>
 
     </div>
   )
+}
+
+
+export const getStaticPaths = async () => {
+
+  return {
+    paths: [],
+    fallback: true
+  }
+}
+
+
+export const getStaticProps = async (context) => {
+  console.log(context)
+
+  return {
+    props: {}
+  }
+
 }
 
 export default StoryPage
