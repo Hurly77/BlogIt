0318252a47a40bf89824e3e0ff3dc23d9354790a
Cameron Leverett <camrbo@gmail.com>
2021-05-19 21:09:22 -0700

    added firs mock page content file

--- components/stories/story-page/story-content.js
+++ components/stories/story-page/story-content.js
@@ -1,12 +1,21 @@
-import Image from 'next/image'
+import { StoryHead } from '@/stories/*'
+import ReactMarkdown from 'react-markdown'
 
-const StoryConent = () => {
+const dummy = {
+  title: 'My First Story',
+  image: '/images/fail.jpg',
+  slug: '/journal/my-first-story',
+  data: '01-02-2021',
+  content: '# This is my first post pretty cool, no?'
+}
+
+const StoryContent = () => {
   return (
     <article>
-      <StoryHead />
-      <p>{content}</p>
+      <StoryHead image={dummy.image} title={dummy.title} />
+      <ReactMarkdown>content</ReactMarkdown>
     </article>
   )
 }
 
-export default StoryConent
+export default StoryContent
